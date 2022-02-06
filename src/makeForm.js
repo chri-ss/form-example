fd2c2cc8ebdb2275711d877cff53b22b8461d7ea
Chris Cresswell <christorper@gmail.com>
2022-02-05 22:53:04 -0800

    Add submit button, required attributr to inputs

--- src/makeForm.js
+++ src/makeForm.js
@@ -2,13 +2,7 @@ const content = document.querySelector("#content");
 const form = document.createElement("form");
 
 const makeForm = () => {
-  const fields = [
-    "Email",
-    "Country",
-    "ZIP",
-    "Password",
-    "Confirm",
-  ];
+  const fields = ["Email", "Country", "ZIP", "Password", "Confirm"];
 
   fields.forEach((field) => {
     const fieldSet = document.createElement("fieldset");
@@ -18,12 +12,16 @@ const makeForm = () => {
     label.textContent = field;
 
     const input = document.createElement("input");
+    input.required = true;
 
     fieldSet.appendChild(label);
     fieldSet.appendChild(input);
     form.appendChild(fieldSet);
   });
-
+  const submit = document.createElement("button");
+  submit.textContent = "submit";
+  submit.type = "submit";
+  form.appendChild(submit);
   content.appendChild(form);
 };
 
